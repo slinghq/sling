6dc4bacf3e5080cfb2bed9f2d3dc642023a8602e
Jesse Panganiban <me@jpanganiban.com>
2015-12-07 23:38:52 +0800

    Introduce yourself, ExampleModule.

--- examples/module/sling/ext/example/__init__.py
+++ examples/module/sling/ext/example/__init__.py
@@ -4,7 +4,7 @@ __version__ = '0.1.0'
 class HelloResource(object):
 
     def on_get(self, req, res):
-        res.body = 'Hello'
+        res.body = "Hello! I'm ExampleModule."
 
 
 hello_resource = HelloResource()
